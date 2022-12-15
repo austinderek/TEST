ce65373fc18dfe1d1f67c54a03ca4e7b694ef9a1
derek <derek@hackerone.com>
2022-12-15 12:34:09 -0600

    add getSport

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,3 +1,14 @@
 const HelloWorld = () => {
   return "Hello World";
 };
+
+const getSport = (id) => {
+  switch (id) {
+    case 1:
+      return "basketball";
+    case 2:
+      return "soccer";
+    default:
+      return "no sport found.";
+  }
+};
