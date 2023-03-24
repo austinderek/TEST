bb17a5ffb281095d6b09c783c1fe1e28a89f39e9
derek <derek@hackerone.com>
2023-03-24 11:53:52 -0500

    init limited app review

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,3 +1,16 @@
 const HelloWorld = () => {
   return "Hello World";
 };
+
+const getSport = (id) => {
+  switch (id) {
+    case 1:
+      return "Basketball";
+    case 2:
+      return "Baseball";
+    case 3:
+      return "Football";
+    default:
+      return "No sport found.";
+  }
+};
